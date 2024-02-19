7b5d1c4dd3f405744b1ea9748fc915d57c74f6c7
Minstrel157 <mamonov_nikita2006@mail.ru>
2024-02-19 15:33:19 +0300

    styles

--- createx/src/parts/Navbar/Navbar.jsx
+++ createx/src/parts/Navbar/Navbar.jsx
@@ -1,12 +1,13 @@
 import React from "react";
-
+import "./Navbar.css"
+import logo from "../../images/navbarImages/logo.svg"
 const Navbar = () =>
 {
 
     return(
         <div className="navbar">
             <div className="leftPart">
-
+                <img src={logo} alt="kurwa" />
             </div>
             <div className="rightPart">
                 
