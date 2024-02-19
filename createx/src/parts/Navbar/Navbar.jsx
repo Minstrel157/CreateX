69669a5e7d6f6374f8cbe392c98f4f5de7ed8a14
Minstrel157 <mamonov_nikita2006@mail.ru>
2024-02-19 14:36:01 +0300

    created new files

---
+++ createx/src/parts/Navbar/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Navbar = () =>
+{
+
+    return(
+        <div className="navbar">
+            <div className="leftPart">
+
+            </div>
+            <div className="rightPart">
+                
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
