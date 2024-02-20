cf54e67df1a4b97a0d3bf851948a96fab1fc83a6
Minstrel157 <mamonov_nikita2006@mail.ru>
2024-02-20 10:38:07 +0300

    create navbar component

--- createx/src/parts/Navbar/Navbar.jsx
+++ createx/src/parts/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Navbar.css"
 import logo from "../../images/navbarImages/logo.svg"
+import phone from "../../images/navbarImages/iPhone.svg"
 const Navbar = () =>
 {
 
@@ -8,9 +9,31 @@ const Navbar = () =>
         <div className="navbar">
             <div className="leftPart">
                 <img src={logo} alt="kurwa" />
+                <div className="links">
+                    <a href="">
+                        About us
+                    </a>
+                    <a href="">
+                        Services
+                    </a>
+                    <a href="">
+                        Work
+                    </a>
+                    <a href="">
+                        News
+                    </a>
+                    <a href="">
+                        Contact
+                    </a>
+                </div>
             </div>
             <div className="rightPart">
-                
+                <div className="telephone">
+                    <img src={phone} alt="" srcset="" />
+                </div>
+                <div className="talkUs">
+
+                </div>
             </div>
         </div>
     )
