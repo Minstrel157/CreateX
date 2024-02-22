4abf41dda5473c8025646250b9e0a3c7c8ee3fdc
Minstrel157 <mamonov_nikita2006@mail.ru>
2024-02-22 11:33:25 +0300

    .

--- createx/src/parts/aboutUs/AboutUs.jsx
+++ createx/src/parts/aboutUs/AboutUs.jsx
@@ -1,20 +1,44 @@
 import React from "react";
 import "./AboutUs.css"
+import video from "../../images/aboutUs/video.svg"
 const AboutUs = () =>
 {
     return(
-    <div className="constructions">
-        <div className="text">
-          <span className="companyName">CREATE
-            <span className='x'>X</span> 
-            <br></br>CONSTRUCTIONS
-          </span>
-          <span className='latinText'>Cras ultrices leo vitae non viverra. Fringilla nisi quisque<br></br> consequat, dignissim vitae proin ipsum sed. Pellentesque nec<br></br> turpis purus eget pellentesque integer ipsum elementum felis.</span>
-        </div>
-        <div className="learn_and_submit">
-            <button className="learn_more">LEARN MORE ABOUT US</button>
-            <button className="submit_request">SUBMIT REQUEST</button>
-        </div>
+    <div className="container">
+      <div className="constructions">
+          <div className="text">
+            <span className="companyName">CREATE
+              <span className='x'>X</span> 
+              <br></br>CONSTRUCTIONS
+            </span>
+            <span className='latinText'>Cras ultrices leo vitae non viverra. Fringilla nisi quisque<br></br> consequat, dignissim vitae proin ipsum sed. Pellentesque nec<br></br> turpis purus eget pellentesque integer ipsum elementum felis.</span>
+          </div>
+          <div className="learn_and_submit">
+              <button className="learn_more">LEARN MORE ABOUT US</button>
+              <button className="submit_request">SUBMIT REQUEST</button>
+          </div>
+          <div className="pages">
+            <a href="#">
+              01
+            </a>
+            <a href="#">
+              02
+            </a>
+            <a href="#">
+              03
+            </a>
+            <a href="#">
+              04
+            </a>
+          </div>
+      </div>
+      <div className="video">
+        <h1>
+          We are Createx Construction Bureau
+        </h1>
+        <span>We are rightfully considered to be the best construction company in the USA.</span>
+        <img src={video} alt="" />
+      </div>
     </div>
     )
 }
